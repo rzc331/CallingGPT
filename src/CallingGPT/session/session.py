dc34abf6c2bd72eb18298589a1fa20edc3e9b2d2
Zhichu Ren <mewPap-fotga5-rawqux>
2023-11-08 14:36:39 -0500

    feat: add support for openai 1.1.1 tool calling

--- src/CallingGPT/session/session.py
+++ src/CallingGPT/session/session.py
@@ -1,9 +1,10 @@
 import json
 import logging
 
-import openai
+from openai import OpenAI
+from openai.types.chat.chat_completion import ChatCompletion
+from openai.types.chat.chat_completion_message import ChatCompletionMessageToolCall
 
-from utils.utils import print_gpt_process
 from ..entities.namespace import Namespace
 
 
@@ -12,15 +13,16 @@ class Session:
 
     messages: list[dict] = []
 
-    model: str = "gpt-3.5-turbo-0613"
+    model: str = "gpt-4-1106-preview"
 
     def __init__(
             self,
             modules: list,
-            model: str = "gpt-3.5-turbo-16k",
+            model: str = model,
             system_prompt: str = "",
             **kwargs
     ):
+        self.client = OpenAI()
         self.namespace = Namespace(modules)
         self.model = model
         self.messages.append(
@@ -40,7 +42,7 @@ class Session:
             self.args['functions'] = self.namespace.functions_list
             self.args['function_call'] = "auto"
 
-    def ask(self, msg: str, fc_chain: bool = True) -> dict:
+    def ask(self, msg: str, fc_chain: bool = True) -> str:
         self.messages.append(
             {
                 "role": "user",
@@ -48,93 +50,97 @@ class Session:
             }
         )
 
-        print_gpt_process(msg, 'msg_received')
+        self.print_gpt_process(msg, 'msg_received')
 
-        resp = openai.ChatCompletion.create(
+        resp = self.client.chat.completions.create(
             **self.args
         )
         self.resp_log.append(resp)
 
         logging.debug("Response: {}".format(resp))
-        reply_msg = resp["choices"][0]['message']
-
-        ret = {}
+        reply_msg = resp.choices[0].message
 
         if fc_chain:
-            while 'function_call' in reply_msg:
-                resp = self.fc_chain(reply_msg['function_call'])
-                reply_msg = resp["choices"][0]['message']
+            # while 'function_call' in reply_msg:
+            while reply_msg.tool_calls and any([tc.type == 'function' for tc in reply_msg.tool_calls]):
+                resp = self.fc_chain(reply_msg.tool_calls)
+                reply_msg = resp.choices[0].message
             ret = {
                 "type": "message",
-                "value": reply_msg['content'],
+                "value": reply_msg.content,
             }
 
             self.messages.append({
                 "role": "assistant",
-                "content": reply_msg['content']
+                "content": reply_msg.content
             })
 
-            print_gpt_process(reply_msg['content'], 'msg_replied')
+            self.print_gpt_process(reply_msg.content, 'msg_replied')
 
             return ret['value']
 
         else:
             if 'function_call' in reply_msg:
 
-                fc = reply_msg['function_call']
-                args = json.loads(fc['arguments'])
-                call_ret = self._call_function(fc['name'], args)
+                fc = reply_msg.function_call
+                args = json.loads(fc.arguments)
+                call_ret = self._call_function(fc.name, args)
 
                 self.messages.append({
                     "role": "function",
-                    "name": fc['name'],
+                    "name": fc.name,
                     "content": str(call_ret)
                 })
 
                 ret = {
                     "type": "function_call",
-                    "func": fc['name'].replace('-', '.'),
+                    "func": fc.name.replace('-', '.'),
                     "value": call_ret,
                 }
             else:
                 ret = {
                     "type": "message",
-                    "value": reply_msg['content'],
+                    "value": reply_msg.content,
                 }
 
                 self.messages.append({
                     "role": "assistant",
-                    "content": reply_msg['content']
+                    "content": reply_msg.content
                 })
 
             return ret['value']
 
-    def fc_chain(self, fc_cmd: dict):
+    def fc_chain(self, fc_cmd_list: list[ChatCompletionMessageToolCall]) -> ChatCompletion:
         """
         Excecute the function call and return the result to ChatGPT.
 
         Args:
-            fc_cmd(dict): The function call command.
+            fc_cmd_list (list[ChatCompletionMessageToolCall]): The function call list from ChatGPT.
 
         Returns:
             dict: The response from ChatGPT.
         """
-        fc_args = json.loads(fc_cmd['arguments'])
-        content = f"function name:\n" \
-                  f"{fc_cmd['name']}\n\n" \
-                  f"args:\n" \
-                  f"{fc_cmd['arguments']}"
-        print_gpt_process(content, 'func_called')
+        for fc_cmd in fc_cmd_list:
+            if fc_cmd.type == 'function':
+                content = f"""
+                function name:\n
+                {fc_cmd.function.name}\n\n
+                args:\n
+                {fc_cmd.function.arguments}
+                """
+                self.print_gpt_process(content, 'func_called')
+                fc_args = json.loads(fc_cmd.function.arguments)
 
-        call_ret = self._call_function(fc_cmd['name'], fc_args)
-        print_gpt_process(f"ret_value:\n{call_ret}", 'func_completed')
+                call_ret = self._call_function(fc_cmd.function.name, fc_args)
+                self.print_gpt_process(f"ret_value:\n{call_ret}", 'func_completed')
 
-        self.messages.append({
-            "role": "function",
-            "name": fc_cmd['name'],
-            "content": str(call_ret)
-        })
-        resp = openai.ChatCompletion.create(
+                self.messages.append({
+                    "tool_call_id": fc_cmd.id,
+                    "role": "tool",
+                    "name": fc_cmd.function.name,
+                    "content": str(call_ret)
+                })
+        resp = self.client.chat.completions.create(
             **self.args
         )
         self.resp_log.append(resp)
@@ -143,6 +149,19 @@ class Session:
 
     def _call_function(self, function_name: str, args: dict):
         return self.namespace.call_function(function_name, args)
+
+    @staticmethod
+    def print_gpt_process(content, gpt_process):
+        assert gpt_process in ['msg_received', 'msg_replied', 'func_called', 'func_completed']
+        mapping = {
+            'msg_received': 'MESSAGE RECEIVED',
+            'msg_replied': 'MESSAGE REPLIED',
+            'func_called': 'FUNCTION CALLED',
+            'func_completed': 'FUNCTION COMPLETED',
+        }
+        print(f"\n****{mapping[gpt_process]}****\n\n"
+              f"{content}\n\n"
+              f"-----------------------\n")
 
 
 class GPT_Session_Handler:
